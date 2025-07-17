24fbd35fb782bd8eb87d474b0e35e3e18af6a814
code4Keerthi <mallelakeerthichandrika@gmail.com>
2025-07-17 06:34:56 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         // Initialize and start the application
         ConsoleUI app = new ConsoleUI();
-
+        app.start();
     }
 }
